dfbf186e8f78b0028b3ccd704f1f703630d97d07
greypixel <greypixel@gpxl.tech>
2024-03-15 09:57:11 +0000

    Web3 Modal

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./globals.css";
 import { ThemeProvider } from "./features/theme/ThemeProvider.tsx";
+import { Web3ModalProvider } from "./features/wallet/Web3ModalProvider.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <App />
+      <Web3ModalProvider>
+        <App />
+      </Web3ModalProvider>
     </ThemeProvider>
   </React.StrictMode>,
 );
