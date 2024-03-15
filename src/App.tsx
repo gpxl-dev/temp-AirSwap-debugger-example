807a8a9a755044102faa6f496877a3b4fec0c6cf
greypixel <greypixel@gpxl.tech>
2024-03-15 09:52:03 +0000

    vite, tailwind, shadcnui

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/features/ui/button";
+
+function App() {
+  return (
+    <main className="container h-full">
+      <Button className="m-4">Click me</Button>
+    </main>
+  );
+}
+
+export default App;
