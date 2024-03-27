1e436ee36321ce8fb781974b7c39de3fc81dbd3e
greypixel <greypixel@gpxl.tech>
2024-03-27 00:45:43 +0000

    Example

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,89 @@
 import { Button } from "@/features/ui/button";
+import { useState } from "react";
+import { cn } from "./lib/utils";
+import { useCheckOrder } from "./useCheckOrder";
+import { useValidateOrder } from "./useValidateOrder";
 
 function App() {
+  const [urlMode, setUrlMode] = useState<boolean>(false);
+
+  // NOTE: would probably be nicer to default this to
+  const [selectedChainId, setselectedChainId] = useState<number>(1);
+
+  const [orderText, setOrderText] = useState<string>("");
+
+  const {
+    orderErrors,
+    contractCallError,
+    orderParsingError,
+    schemaValidationError,
+  } = useValidateOrder({
+    order: orderText,
+    isUrl: urlMode,
+    onSetChain: (newId) => {
+      if (selectedChainId !== newId) setselectedChainId(newId);
+    },
+  });
+
+  console.log({
+    orderErrors,
+    contractCallError,
+    orderParsingError,
+    schemaValidationError,
+  });
+
   return (
-    <main className="container h-full">
-      <Button className="m-4">Click me</Button>
+    <main className="container flex h-full flex-col gap-4">
+      <h1>Order checker</h1>
+      <div className="flex gap-2">
+        <button
+          onClick={() => setUrlMode(false)}
+          className={cn({
+            underline: !urlMode,
+          })}
+        >
+          text mode
+        </button>
+        <button
+          onClick={() => setUrlMode(true)}
+          className={cn({
+            underline: urlMode,
+          })}
+        >
+          URL mode
+        </button>
+      </div>
+
+      {urlMode ? (
+        <div>
+          <input type="text" placeholder="Enter URL" className="w-full" />
+          <Button>Check</Button>
+        </div>
+      ) : (
+        <div>
+          <textarea
+            value={orderText}
+            onChange={(e) => setOrderText(e.target.value)}
+            placeholder="Enter order text"
+            className="w-full text-black"
+            rows={10}
+          />
+          <Button>Check</Button>
+        </div>
+      )}
+
+      <pre className="whitespace-pre">
+        {JSON.stringify(
+          {
+            orderErrors,
+            contractCallError,
+            orderParsingError,
+            schemaValidationError,
+          },
+          null,
+          2,
+        )}
+      </pre>
     </main>
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@ import path from "path";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 import { ValidateEnv } from "@julr/vite-plugin-validate-env";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 export default defineConfig({
-  plugins: [react(), ValidateEnv()],
+  plugins: [react(), ValidateEnv(), nodePolyfills()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
